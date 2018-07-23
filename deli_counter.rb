821ef42601bd1f80591277152261c200ea180a06
Richard Chen <richard.c.chen@gmail.com>
2018-07-23 00:29:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 katz_deli = []
+
 def line(array)
-  if line.size() == 0
+  if array.size() == 0
     puts "The line is currently empty."
   else
     puts "The is currently: #{array}"
