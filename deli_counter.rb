e51db8bfeb85bc96a8efa59dfe40c13d2b5fa018
Richard Chen <richard.c.chen@gmail.com>
2018-07-23 00:29:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.size() == 0
     puts "The line is currently empty."
   else
-    puts "The is currently: #{array}"
+    puts "The line is currently: #{array}"
   end
     
 end
