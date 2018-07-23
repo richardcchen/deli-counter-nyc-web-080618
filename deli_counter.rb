7e04d7e6b5ec39c27f261366d6e214e6cda4c7a8
Richard Chen <richard.c.chen@gmail.com>
2018-07-23 01:35:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,5 +13,9 @@ def line(array)
     end
     puts lin
   end
-    
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length+1} in line."
 end
