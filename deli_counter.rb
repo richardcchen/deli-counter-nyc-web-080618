3958a77e53b8954a53c816e2c7e7e37fc917957d
Richard Chen <richard.c.chen@gmail.com>
2018-07-23 01:04:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,9 @@ def line(array)
     puts "The line is currently empty."
   else
     counter = 0
-      lin = "The line is currently: "
+      lin = "The line is currently:"
     array.each do |i|
-      lin = lin + "#{counter + 1}. #{i} "
+      lin = lin + " #{counter + 1}. #{i}"
       counter += 1
     end
     puts lin
